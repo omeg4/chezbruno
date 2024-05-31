a1b14e790a5808649ffced842470afdb575ba6c3
Matt <matthew.brunetti28@gmail.com>
2024-05-31 13:18:05 -0400

    Update .config/lazygit/state.yml Update .config/nvim/lua/bruno/plugins/telescope.lua Update .config/fish/fish_plugins Update smb.conf

--- dot_config/nvim/lua/bruno/plugins/telescope.lua
+++ dot_config/nvim/lua/bruno/plugins/telescope.lua
@@ -7,7 +7,7 @@ return {
 		"nvim-tree/nvim-web-devicons",
 	},
 	config = function()
-		local trouble = require("trouble.providers.telescope")
+		local trouble = require("trouble.sources.telescope")
 		local actions = require("telescope.actions")
 		local telescope = require("telescope")
 
@@ -19,12 +19,12 @@ return {
 						["<C-k>"] = actions.move_selection_previous, -- move to prev result
 						["<C-j>"] = actions.move_selection_next, -- move to next result
 						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-						["<C-t>"] = trouble.open_with_trouble,
+						["<C-t>"] = trouble.open,
             ["<C-v>"] = actions.file_split,
             ["<C-i>"] = actions.file_vsplit,
 					},
 					n = {
-						["<C-t>"] = trouble.open_with_trouble,
+						["<C-t>"] = trouble.open,
             ["<C-v>"] = actions.file_split,
             ["<C-i>"] = actions.file_vsplit,
 					},
